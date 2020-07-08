6c205a6b049d42229d5e1fdb9e58efc4cdee4a46
ZeroYl <ZeroYl@DESKTOP-F7M4OIO>
2020-07-08 21:25:58 +0800

    test

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            String str =  Console.ReadLine();
+            Console.WriteLine("Hello {0}", str);
+            Console.WriteLine(typeof(String));
+            Console.ReadLine();
+        }
+    }
+}
